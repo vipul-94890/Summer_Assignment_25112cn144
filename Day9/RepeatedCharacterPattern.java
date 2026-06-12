64fe67a80f74a581113f1102eed8ee612205b5ba
Ved Prakash <mishravipul301@gmail.com>
2026-06-12 20:49:36 +0530

    Add Day9 Java programs

---
+++ Day9/RepeatedCharacterPattern.java
@@ -0,0 +1,13 @@
+public class RepeatedCharacterPattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            char ch = (char)('A' + i - 1);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+}
